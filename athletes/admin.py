db525ab0e8b2ed99b5a5f775c1e34cfa960b6f93
Magda <magda.mejia96@gmail.com>
2020-02-01 20:52:31 -0500

    gitignore changes and media config added, refactor models in admin.py

--- athletes/admin.py
+++ athletes/admin.py
@@ -3,11 +3,11 @@ from .models import *
 
 # Register your models here.
 @admin.register(Trainer)
-class AdminCity(admin.ModelAdmin):
+class AdminTrainer(admin.ModelAdmin):
     list_display = ('id', 'first_name', 'last_name')
 
 @admin.register(Athlete)
-class AdminCity(admin.ModelAdmin):
+class AdminAthlete(admin.ModelAdmin):
     list_display = ('id', 'first_name', 'last_name', 'birth_place')
 
 
