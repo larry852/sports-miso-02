f5365e33543f283a7c4190bf203ed7d7d06103da
MateoLeon <mateoleon127@gmail.com>
2020-02-01 22:51:16 -0500

    Arreglado Commentary, tiene ahora usuario

--- core/settings.py
+++ core/settings.py
@@ -117,10 +117,6 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.0/howto/static-files/
-
-STATIC_URL = '/static/'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
