6487ee84d19d769851b377923d7165c93dc1578a
MateoLeon <mateoleon127@gmail.com>
2020-02-01 23:10:49 -0500

    Solved conflict, null=False in youtube_id for models.py

--- participations/models.py
+++ participations/models.py
@@ -9,7 +9,7 @@ User = get_user_model()
 # Create your models here.
 class Participation(models.Model):
     datetime = models.DateField(null=False)
-    youtube_id = models.URLField(max_length=200, null=True)
+    youtube_id = models.URLField(max_length=200, null=False)
     result = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(5)], null=False)
     athlete_id =  models.ForeignKey('athletes.Athlete', on_delete = models.CASCADE)
 #TODO: falta modality_id
@@ -17,10 +17,5 @@ class Participation(models.Model):
 class Commentary(models.Model):
     comment = models.CharField(max_length=200, null=False, blank=False)
     datetime = models.DateField(null=False)
-<<<<<<< HEAD
     participation = models.ForeignKey('Participation', on_delete = models.CASCADE)
     user = models.ForeignKey(User, on_delete = models.CASCADE)
-=======
-    participation_id = models.ForeignKey('Participation', on_delete = models.CASCADE)
-    user = models.ForeignKey(User, on_delete = models.CASCADE)
->>>>>>> 6c99e6b81125b349919b9cb3a62b692357b8d8e3
