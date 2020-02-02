fa4528ffcb4f1b5af99173d058a169f347589eb0
magda26 <magda_mejia96@hotmail.com>
2020-02-01 19:13:07 -0500

    Update athletes/admin.py
    Co-Authored-By: Larry <larry852@users.noreply.github.com>

--- athletes/admin.py
+++ athletes/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-admin.site.register(Trainer)
+@admin.register(Trainer)
+class AdminCity(admin.ModelAdmin):
+    list_display = ('id', 'first_name', 'last_name')
 
 admin.site.register(Athlete)
