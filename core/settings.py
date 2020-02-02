5904aa79310379365ed361e17732e4d190a7ca89
larry852 <larryportocarrero@gmail.com>
2020-02-01 20:44:03 -0500

    Add config heroku - static

--- core/settings.py
+++ core/settings.py
@@ -118,3 +118,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# ---------------------- Heroku -------------------------------
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+# ---------------------- Heroku -------------------------------
