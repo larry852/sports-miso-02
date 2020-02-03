89ef54ece345e1ca62b059f133ca4a52bcac0c50
larry852 <larryportocarrero@gmail.com>
2020-02-02 20:17:35 -0500

    Add weight field

--- athletes/models.py
+++ athletes/models.py
@@ -17,6 +17,7 @@ class Athlete (models.Model):
     first_name = models.CharField(max_length=40, null=False, blank=False)
     last_name = models.CharField(max_length=40, null=True, blank=False)
     height = models.FloatField()
+    weight = models.FloatField()
     birth_date = models.DateField(null=True, blank=True)
     birth_place = models.CharField(max_length=40, null=True, blank=False)
     picture = models.ImageField(upload_to=get_path_class, default='default-profile.png')
