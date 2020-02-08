73d9dcc5a78e91763aa17d7a824a7146d33f7466
larry852 <larryportocarrero@gmail.com>
2020-02-08 14:07:13 -0500

    Enable session auth for api view

--- core/settings.py
+++ core/settings.py
@@ -172,5 +172,6 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
     ]
 }
