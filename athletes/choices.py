b233137f1de9e32736470a2890d0611d52642a4d
larry852 <larryportocarrero@gmail.com>
2020-02-08 01:04:29 -0500

    Add enum for cities

---
+++ athletes/choices.py
@@ -0,0 +1,10 @@
+CITY_CHOICES = (
+    ('001', 'Bogotá'),
+    ('002', 'Pasto'),
+    ('003', 'Tunja'),
+    ('004', 'Barranquilla'),
+    ('005', 'Medellín'),
+    ('007', 'Cartagena'),
+    ('008', 'Manizales'),
+    ('009', 'Yopal'),
+)
