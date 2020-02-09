26562311a2d92a1aca41e5ccc36490d794497704
Magda <magda.mejia96@gmail.com>
2020-02-09 10:42:40 -0500

    Remove unused comments

--- core/settings.py
+++ core/settings.py
@@ -169,7 +169,7 @@ STATICFILES_DIRS = [
 STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-DEFAULT_FILE_STORAGE = environ.get('DEFAULT_FILE_STORAGE', 'django.files.storage.FileSystemStorage')   # 'core.storage_backends.MediaStorage'
+DEFAULT_FILE_STORAGE = environ.get('DEFAULT_FILE_STORAGE', 'django.files.storage.FileSystemStorage')
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
