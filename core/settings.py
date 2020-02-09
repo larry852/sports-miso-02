fb9fed484c23a04f59d6a626e5574c15d7ba3e22
Magda <magda.mejia96@gmail.com>
2020-02-09 11:03:27 -0500

    warning manage added

--- core/settings.py
+++ core/settings.py
@@ -162,7 +162,7 @@ AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
 }
 AWS_LOCATION = 'static'
-
+AWS_DEFAULT_ACL = None
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
