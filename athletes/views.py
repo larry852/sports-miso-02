7ed7b863c60350c0663040fc22c8dd8f82797d01
larry852 <larryportocarrero@gmail.com>
2020-02-16 00:31:36 -0500

    :sparkles: Add participations to detail of athlete

--- athletes/views.py
+++ athletes/views.py
@@ -1,9 +1,16 @@
 from rest_framework import viewsets
 from .models import Athlete
-from .serializers import AthleteSerializer
+from .serializers import AthleteSerializer, DetailAthleteSerializer
 
 
 class AthleteViewSet(viewsets.ModelViewSet):
     queryset = Athlete.objects.all().order_by('id')
     serializer_class = AthleteSerializer
+    detail_serializer_class = DetailAthleteSerializer
     http_method_names = ['get']
+
+    def get_serializer_class(self):
+        if self.action == 'retrieve':
+            if hasattr(self, 'detail_serializer_class'):
+                return self.detail_serializer_class
+        return self.serializer_class
