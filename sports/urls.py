ac361627c5c257309647e626793bb22443544337
Juan Sebastian Alvarez Eraso <juan_alvarez_12345@hotmail.com>
2020-02-22 17:58:17 -0500

    Improvement location for urls

--- sports/urls.py
+++ sports/urls.py
@@ -1,6 +1,6 @@
-
 from rest_framework import routers
-from athletes.views import AthleteViewSet
+
+from sports.views import SportViewSet
 
 router = routers.DefaultRouter()
 router.register('sports', SportViewSet)
